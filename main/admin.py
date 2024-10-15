f7ee698d0a0ea1eda7932049e20b94dc342014cc
Henrique <sdbarbosa_haiti@hotmail.com>
2024-10-14 21:08:40 -0300

    configurando permissões de usuários no banco

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Dono, Balcao, Frutas
 
-# Register your models here.
+admin.site.register(Dono)
+admin.site.register(Balcao)
+admin.site.register(Frutas)
--- main/models.py
+++ main/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class Dono(models.Model):
+    nome = models.CharField(max_length=50)
+    sobrenome = models.CharField(max_length=50)
+    balcao = models.ForeignKey('Balcao', on_delete=models.CASCADE)
+    frutas = models.ForeignKey('Frutas', on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.nome
+    
+class Balcao(models.Model):
+    nome_balcao = models.CharField(max_length=50)
+    def __str__(self):
+        return self.nome_balcao
+
+class Frutas(models.Model):
+    nome_fruta = models.CharField(max_length=250)
+    def __str__(self):
+        return self.nome_fruta
+    
